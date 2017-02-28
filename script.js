9b282c345b44c70a83a7734e482b323570ef6815
brianfajardo <bmf5@hotmail.com>
2017-02-28 01:00:38 -0500

    restructure with Promise

--- script.js
+++ script.js
@@ -5,22 +5,52 @@ let pageCounter = 1;
 let fetchButton = document.querySelector('#btn');
 let animalContainer = document.querySelector('#animal-info');
 
-fetchButton.addEventListener('click', () => {
-    let ourRequest = new XMLHttpRequest();
-    // GET: download/receive data, 
-    // POST: send data to server
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter + '.json', true);
-    ourRequest.onload = () => {
-        let ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData);
-        renderHTML(ourData);
-    };
-ourRequest.send();
-pageCounter++;
-if (pageCounter > 3){
-    fetchButton.classList.add('hide-me');
-}
-});
+// fetchButton.addEventListener('click', () => {
+//     let ourRequest = new XMLHttpRequest();
+//     // GET: download/receive data, 
+//     // POST: send data to server
+//     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter + '.json', true);
+//     ourRequest.onload = () => {
+//         let ourData = JSON.parse(ourRequest.responseText);
+//         console.log(ourData);
+//         renderHTML(ourData);
+//     };
+// ourRequest.send();
+// pageCounter++;
+// if (pageCounter > 3){
+//     fetchButton.classList.add('hide-me');
+// }
+// });
+
+fetchButton.addEventListener('click', promiseThen);
+
+function promiseThen(){
+    fetchData()
+        .then(responseData => {
+            renderHTML(responseData);
+        })
+        .catch(error => console.log(error));
+};
+
+function fetchData(){
+    return new Promise((resolve, reject) => {
+        const REQUEST = new XMLHttpRequest();
+        // GET: download/receive data, 
+        // POST: send data to server
+        REQUEST.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter + '.json', true);
+        REQUEST.onreadystatechange = () => {
+            if (REQUEST.readyState === XMLHttpRequest.DONE && REQUEST.status === 200 && REQUEST.status < 400){
+                const RESPONSE_DATA = JSON.parse(REQUEST.responseText);
+                resolve(RESPONSE_DATA);
+            }
+        }
+        REQUEST.send();
+        pageCounter++;
+        if (pageCounter > 3){
+            fetchButton.classList.add('hide-me');
+        }
+    })
+};
 
 function renderHTML(data){
     let htmlString = '';
@@ -35,7 +65,6 @@ function renderHTML(data){
                 htmlString += ' and ' + data[i].foods.likes[ii]
             }
         }
-
         htmlString += ' and dislikes ';
         // Loop through nested array object of property in JSON.parse array object[i]
         for (let ii = 0; ii < data[i].foods.dislikes.length; ii++){
@@ -45,9 +74,7 @@ function renderHTML(data){
                 htmlString += ' and ' + data[i].foods.dislikes[ii]
             }
         }
-
         htmlString += '.</p>';
     };
-
     animalContainer.insertAdjacentHTML('beforeend', htmlString);
 };

5d9bc2a644762eaf823087eada9f7058b7dcdb69
brianfajardo <bmf5@hotmail.com>
2017-02-27 23:57:42 -0500

    finish app

---
+++ script.js
@@ -0,0 +1,53 @@
+// JSON = JavaScript Object Notation
+// AJAX = Asynchronous JavaScript And XML
+
+let pageCounter = 1;
+let fetchButton = document.querySelector('#btn');
+let animalContainer = document.querySelector('#animal-info');
+
+fetchButton.addEventListener('click', () => {
+    let ourRequest = new XMLHttpRequest();
+    // GET: download/receive data, 
+    // POST: send data to server
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-'+ pageCounter + '.json', true);
+    ourRequest.onload = () => {
+        let ourData = JSON.parse(ourRequest.responseText);
+        console.log(ourData);
+        renderHTML(ourData);
+    };
+ourRequest.send();
+pageCounter++;
+if (pageCounter > 3){
+    fetchButton.classList.add('hide-me');
+}
+});
+
+function renderHTML(data){
+    let htmlString = '';
+    // Loop through JSON.parse array object[i]
+    for (let i = 0; i < data.length; i++){
+        htmlString += '<p>' + data[i].name + ' is a ' + data[i].species + " that likes to eat ";
+        // Loop through nested array object of property in JSON.parse array object[i]
+        for (let ii = 0; ii < data[i].foods.likes.length; ii++){
+            if (ii === 0){
+                htmlString += data[i].foods.likes[ii]
+            } else {
+                htmlString += ' and ' + data[i].foods.likes[ii]
+            }
+        }
+
+        htmlString += ' and dislikes ';
+        // Loop through nested array object of property in JSON.parse array object[i]
+        for (let ii = 0; ii < data[i].foods.dislikes.length; ii++){
+            if (ii === 0){
+                htmlString += data[i].foods.dislikes[ii]
+            } else {
+                htmlString += ' and ' + data[i].foods.dislikes[ii]
+            }
+        }
+
+        htmlString += '.</p>';
+    };
+
+    animalContainer.insertAdjacentHTML('beforeend', htmlString);
+};
